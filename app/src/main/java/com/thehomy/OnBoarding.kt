f7d3a7ee3b02e924429ba8fad66a0b450c908b7d
Shagun <shagunchahar8@gmail.com>
2024-04-09 16:49:11 +0530

    Binding

--- app/src/main/java/com/thehomy/OnBoarding.kt
+++ app/src/main/java/com/thehomy/OnBoarding.kt
@@ -5,21 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.thehomy.databinding.ActivityMainBinding
+import com.thehomy.databinding.ActivityOnBoardingBinding
 
-class MainActivity : AppCompatActivity() {
+class OnBoarding : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    lateinit var binding : ActivityOnBoardingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityOnBoardingBinding.inflate(layoutInflater)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-
     }
 }
