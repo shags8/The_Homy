70f3db31331259a60c2069e03f985ef6bcc10c38
risha <goswamirishabh2411@gmail.com>
2024-04-25 13:46:17 +0530

    tool Bar

---
+++ app/src/main/java/com/thehomy/notification.kt
@@ -0,0 +1,25 @@
+package com.thehomy
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.thehomy.databinding.ActivityNotificationBinding
+
+class notification : AppCompatActivity() {
+    private lateinit var binding: ActivityNotificationBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityNotificationBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.toolBar.textView.text = "Notification"
+    }
+}
